2d71107781b8a50d830b49eaf22a942f6e67f123
jaylfc <jaylfc25@gmail.com>
2026-06-05 14:16:50 +0100

    image: derive Serialize/Deserialize for Rect (fixes ImagePlacement build)

--- src/geometry.rs
+++ src/geometry.rs
@@ -5,7 +5,7 @@ impl Point {
     pub fn new(x: i32, y: i32) -> Self { Self { x, y } }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct Rect { pub x: i32, pub y: i32, pub w: i32, pub h: i32 }
 
 impl Rect {
